11f1382b3bd2b450e7fc441dbddb9bf44c10b553
Aleksandr Brenchev <abrenchev@griddynamics.com>
2020-06-02 13:50:30 +0200

    Implemented SimpleGson for primitives and arrays of primitives

--- hw-L15-serialization/src/main/java/com/abrenchev/gson/SimpleGson.java
+++ hw-L15-serialization/src/main/java/com/abrenchev/gson/SimpleGson.java
@@ -1,22 +1,58 @@
 package com.abrenchev.gson;
 
+import com.abrenchev.gson.exceptions.SimpleGsonException;
+
 import javax.json.Json;
-import javax.json.Json.*;
 import javax.json.JsonArray;
-import javax.json.JsonNumber;
 import javax.json.JsonObject;
-import javax.json.JsonString;
-import javax.json.JsonStructure;
 import javax.json.JsonValue;
+import java.io.StringReader;
+import java.lang.reflect.Array;
+import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
 
 public class SimpleGson {
+    public String toJson(Object obj) {
+        var jsonBuilder = Json.createObjectBuilder();
+
+        for (Field objField : obj.getClass().getDeclaredFields()) {
+            if (!objField.getName().equals("this$0")) {
+                jsonBuilder.add(objField.getName(), getFieldValue(obj, objField));
+            }
+        }
+
+        return jsonBuilder.build().toString();
+    }
+
+    public Object fromJson(String json, Class<?> clazz) {
+        var jsonReader = Json.createReader(new StringReader(json));
+        var jsonObject = jsonReader.readObject();
+
+        Object result = createInstance(clazz);
+
+        for (Field clazzField : clazz.getDeclaredFields()) {
+            setObjectField(result, clazzField, getJsonFieldValue(jsonObject, clazzField));
+        }
+
+        return result;
+    }
+
+    private void setObjectField(Object obj, Field field, Object value) {
+        try {
+            field.setAccessible(true);
+            field.set(obj, value);
+        } catch (IllegalAccessException exception) {
+            throw new SimpleGsonException("Could not set field value of " + field.getName(), exception);
+        }
+    }
+
     private int getIntegerValue(Object obj, Field field) {
         try {
             field.setAccessible(true);
             return (int) field.get(obj);
         } catch (IllegalAccessException exception) {
-            return 0;
+            throw new SimpleGsonException("Could not get an int field of " + field.getName(), exception);
         }
     }
 
@@ -25,7 +61,7 @@ public class SimpleGson {
             field.setAccessible(true);
             return (boolean) field.get(obj);
         } catch (IllegalAccessException exception) {
-            return false;
+            throw new SimpleGsonException("Could not get a boolean field value of " + field.getName(), exception);
         }
     }
 
@@ -34,7 +70,33 @@ public class SimpleGson {
             field.setAccessible(true);
             return (String) field.get(obj);
         } catch (IllegalAccessException exception) {
-            return "";
+            throw new SimpleGsonException("Could not get a string field value of " + field.getName(), exception);
+        }
+    }
+
+    private JsonArray getJsonArray(Object obj, Field field) {
+        try {
+            var arrayBuilder = Json.createArrayBuilder();
+            field.setAccessible(true);
+            var objArray = field.get(obj);
+            var objArrayType = field.getType().getComponentType();
+            var arrayLength = Array.getLength(objArray);
+
+            for (int i = 0; i < arrayLength; i++) {
+                if (objArrayType.equals(Integer.TYPE)) {
+                    arrayBuilder.add(Array.getInt(objArray, i));
+                } else if (objArrayType.equals(Boolean.TYPE)) {
+                    arrayBuilder.add(Array.getBoolean(objArray, i));
+                } else if (objArrayType.equals(String.class)) {
+                    arrayBuilder.add((String) Array.get(objArray, i));
+                } else {
+                    arrayBuilder.add(JsonValue.NULL);
+                }
+            }
+
+            return arrayBuilder.build();
+        } catch (IllegalAccessException exception) {
+            throw new SimpleGsonException("Could not get an array field value of " + field.getName(), exception);
         }
     }
 
@@ -53,23 +115,59 @@ public class SimpleGson {
             return Json.createValue(getStringValue(obj, field));
         }
 
+        if (fieldType.isArray()) {
+            return getJsonArray(obj, field);
+        }
+
         return JsonValue.NULL;
     }
 
-
-    public String toJson(Object obj) {
-        var jsonBuilder = Json.createObjectBuilder();
-
-        for (Field objField : obj.getClass().getDeclaredFields()) {
-            if (!objField.getName().equals("this$0")) {
-                jsonBuilder.add(objField.getName(), getFieldValue(obj, objField));
-            }
+    private Object createInstance(Class<?> clazz) {
+        try {
+            Constructor<?> constructor = clazz.getConstructor();
+            constructor.setAccessible(true);
+            return constructor.newInstance();
+        } catch (NoSuchMethodException | InstantiationException | IllegalAccessException | InvocationTargetException exception) {
+            throw new SimpleGsonException("Could't construct the new instance", exception);
         }
-
-        return jsonBuilder.build().toString();
     }
 
-    public Object fromJson(String json, Class<?> clazz) {
-        return null;
+    private Object getJsonFieldValue(JsonObject obj, Field field) {
+        String fieldName = field.getName();
+        Class<?> fieldType = field.getType();
+
+        if (fieldType.equals(Integer.TYPE)) {
+            return obj.getInt(fieldName);
+        }
+
+        if (fieldType.equals(Boolean.TYPE)) {
+            return obj.getBoolean(fieldName);
+        }
+
+        if (fieldType.equals(String.class)) {
+            return obj.getString(fieldName);
+        }
+
+        if (fieldType.isArray()) {
+            var jsonArray = obj.getJsonArray(fieldName);
+            var arrayType = fieldType.getComponentType();
+            var objArray = Array.newInstance(arrayType, jsonArray.size());
+
+            for (int i = 0; i < jsonArray.size(); i++) {
+                if (arrayType.equals(Integer.TYPE)) {
+                    Array.setInt(objArray, i, jsonArray.getInt(i));
+                } else if (arrayType.equals(Boolean.TYPE)) {
+                    Array.setBoolean(objArray, i, jsonArray.getBoolean(i));
+                } else if (arrayType.equals(String.class)) {
+                    Array.set(objArray, i, jsonArray.getString(i));
+                } else {
+                    Array.set(objArray, i, null);
+                }
+            }
+
+            return objArray;
+        }
+
+        return obj.get(field.getName());
     }
 }
--- hw-L15-serialization/src/test/java/com/abrenchev/testing/services/SimpleGsonTest.java
+++ hw-L15-serialization/src/test/java/com/abrenchev/testing/services/SimpleGsonTest.java
@@ -6,24 +6,34 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.*;
 
+class Animal {
+    public String name;
+    public int age;
+    public boolean isAdopted;
+
+    public Animal() {
+
+    }
+}
+
+class Pizza {
+    public String name;
+    public String[] ingredients;
+
+    public Pizza() {
+
+    }
+}
+
 @DisplayName("SimpleGson")
 public class SimpleGsonTest {
-    class Animal {
-        public String name;
-        public int age;
-        public boolean isAdopted;
-
-        public Animal(String name, int age, boolean isAdopted) {
-            this.name = name;
-            this.age = age;
-            this.isAdopted = isAdopted;
-        }
-    }
-
     @Test
     @DisplayName("should correctly transform object to JSON string with primitive values")
     public void testToJson() {
-        Animal pet = new Animal("Tom", 7, true);
+        Animal pet = new Animal();
+        pet.name = "Tom";
+        pet.age = 7;
+        pet.isAdopted = true;
 
         Gson gson = new Gson();
         SimpleGson simpleGson = new SimpleGson();
@@ -34,11 +44,47 @@ public class SimpleGsonTest {
     @Test
     @DisplayName("should correctly build a new object from JSON string")
     public void testFromJson() {
-        String petJson = "{name: \"Kate\", age: 15, isAdopted: false}";
+        String petJson = "{\"name\": \"Kate\", \"age\": 15, \"isAdopted\": false}";
+        SimpleGson simpleGson = new SimpleGson();
+        Animal parsedAnimal =  (Animal) simpleGson.fromJson(petJson, Animal.class);
+
+        assertThat(parsedAnimal.name).isEqualTo("Kate");
+        assertThat(parsedAnimal.age).isEqualTo(15);
+        assertThat(parsedAnimal.isAdopted).isEqualTo(false);
+    }
+
+    @Test
+    @DisplayName("should correctly transform arrays of primitive values")
+    public void testToJsonPrimitiveArrays() {
+        Pizza pepperoni = new Pizza();
+        pepperoni.name = "Pepperoni";
+        pepperoni.ingredients = new String[]{"salami", "cheese", "tomato"};
 
         Gson gson = new Gson();
         SimpleGson simpleGson = new SimpleGson();
 
-        assertThat(simpleGson.fromJson(petJson, Animal.class)).isEqualTo(gson.fromJson(petJson, Animal.class));
+        assertThat(simpleGson.toJson(pepperoni)).isEqualTo(gson.toJson(pepperoni));
+    }
+
+    @Test
+    @DisplayName("should correctly reads arrays of primitive values")
+    public void testFromJsonPrimitiveValues() {
+        Pizza pepperoni = new Pizza();
+        pepperoni.name = "Pepperoni";
+        pepperoni.ingredients = new String[]{"salami", "cheese", "tomato"};
+
+        Gson gson = new Gson();
+
+        String pizzaJson = gson.toJson(pepperoni);
+        SimpleGson simpleGson = new SimpleGson();
+
+        Pizza parsedPizza = (Pizza) simpleGson.fromJson(pizzaJson, Pizza.class);
+
+        assertThat(parsedPizza.name).isEqualTo(pepperoni.name);
+        assertThat(parsedPizza.ingredients.length).isEqualTo(pepperoni.ingredients.length);
+
+        for (int i = 0; i < parsedPizza.ingredients.length; i++) {
+            assertThat(parsedPizza.ingredients[i]).isEqualTo(pepperoni.ingredients[i]);
+        }
     }
 }
