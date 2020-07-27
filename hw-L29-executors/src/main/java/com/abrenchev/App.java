0f922b443fb7da7d116ed77efe9eb643bb045722
Aleksandr Brenchev <abrenchev@griddynamics.com>
2020-07-27 14:02:09 +0200

    Added project for homework

---
+++ hw-L29-executors/src/main/java/com/abrenchev/App.java
@@ -0,0 +1,12 @@
+package com.abrenchev;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class App {
+    public static void main(String... args) {
+        System.out.println("There will be some multi-threading");
+    }
+}
