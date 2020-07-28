1ca1192a139c4c3525bfa70d9075ece7db1265bc
Aleksandr Brenchev <abrenchev@griddynamics.com>
2020-07-28 10:51:08 +0200

    Maybe it works

--- hw-L29-executors/src/main/java/com/abrenchev/App.java
+++ hw-L29-executors/src/main/java/com/abrenchev/App.java
@@ -1,12 +1,36 @@
 package com.abrenchev;
 
-import java.util.ArrayList;
-import java.util.Collections;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import java.util.Arrays;
 import java.util.List;
-import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.CompletableFuture;
+import java.util.stream.Collectors;
 
 public class App {
+    public static final Logger logger = LoggerFactory.getLogger(App.class);
+
     public static void main(String... args) {
-        System.out.println("There will be some multi-threading");
+        new App().logRange(Arrays.asList(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0));
+    }
+
+    public void logRange(List<Integer> range) {
+        CompletableFuture<Integer> resultFuture = CompletableFuture.completedFuture(0);
+        List<CompletableFuture<Integer>> numberFutures = range.stream()
+                .map((num) -> CompletableFuture.supplyAsync(() -> num)).collect(Collectors.toList());
+
+        for (CompletableFuture<Integer> numFuture : numberFutures) {
+            resultFuture = resultFuture
+                    .thenCombineAsync(numFuture, (empty, num) -> num)
+                    .thenApplyAsync(this::logNumber)
+                    .thenApplyAsync(this::logNumber);
+        }
+
+        resultFuture.join();
+    }
+
+    private int logNumber(int value) {
+        logger.info("Logged value: " + value);
+        return value;
     }
 }
