5189d80b4f589422deda3049783801306e7d78ef
Aleksandr Brenchev <abrenchev@griddynamics.com>
2020-07-30 12:56:31 +0200

    Moved from CompleteableFutures to plaing syncrhonized with wait and notify

--- hw-L29-executors/src/main/java/com/abrenchev/App.java
+++ hw-L29-executors/src/main/java/com/abrenchev/App.java
@@ -4,33 +4,35 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import java.util.Arrays;
 import java.util.List;
-import java.util.concurrent.CompletableFuture;
-import java.util.stream.Collectors;
 
 public class App {
     public static final Logger logger = LoggerFactory.getLogger(App.class);
 
+    private long lastLoggedThreadId;
+
     public static void main(String... args) {
-        new App().logRange(Arrays.asList(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0));
+        var range = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 8, 7, 6, 5, 4, 3, 2, 1);
+        var app = new App();
+
+        new Thread(() -> app.logRange(range)).start();
+        new Thread(() -> app.logRange(range)).start();
     }
 
-    public void logRange(List<Integer> range) {
-        CompletableFuture<Integer> resultFuture = CompletableFuture.completedFuture(0);
-        List<CompletableFuture<Integer>> numberFutures = range.stream()
-                .map((num) -> CompletableFuture.supplyAsync(() -> num)).collect(Collectors.toList());
+    public synchronized void logRange(List<Integer> range) {
+        for (Integer num : range) {
+            try {
+                long threadId = Thread.currentThread().getId();
+                while (lastLoggedThreadId == threadId) {
+                    wait();
+                }
 
-        for (CompletableFuture<Integer> numFuture : numberFutures) {
-            resultFuture = resultFuture
-                    .thenCombineAsync(numFuture, (empty, num) -> num)
-                    .thenApplyAsync(this::logNumber)
-                    .thenApplyAsync(this::logNumber);
+                lastLoggedThreadId = threadId;
+                logger.info("Logging value: " + num);
+                Thread.sleep(1500);
+                notifyAll();
+            } catch (InterruptedException exception) {
+                throw new AppException("Something went wrong", exception);
+            }
         }
-
-        resultFuture.join();
-    }
-
-    private int logNumber(int value) {
-        logger.info("Logged value: " + value);
-        return value;
     }
 }
